31a0add7e0af8aa3009012bbc06f54cd25318d9e
Isaque dos Reis <IsaqueReis@users.noreply.github.com>
2020-04-18 16:02:08 -0300

    Update input_handler.h

--- include/input_handler.h
+++ include/input_handler.h
@@ -2,6 +2,6 @@
 //se o número de caracteres lidos for maior do que 
 //o limite do buffer, mais espaço é alocado para string.
 //se o usuário teclar RET ou for o final do arquivo, 
-//é colocado o delimitador de string '\0' e retornada 
+//é colocado o delimitador de string '\0' e é retornada
 //a string digitada pelo usuário.
 char *read_line();
